34b51b500b2c3c89247d8415319d41693177b5b0
israil-fakir <israilfakirrobin@gmail.com>
2025-07-11 09:43:23 +0600

    remove the base from vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/To_do_list_with_weather/',
+  // base: '/To_do_list_with_weather/',
 })
